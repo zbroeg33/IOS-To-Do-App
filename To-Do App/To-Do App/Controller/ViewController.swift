47cbcfddd443986477df63c3287b35f6c00180d4
Zacharybroeg <Zacharybroeg@u.boisestate.edu>
2018-07-12 21:37:59 -0600

    Initial Commit of Project

---
+++ To-Do App/To-Do App/Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  To-Do App
+//
+//  Created by Zachary Broeg on 7/12/18.
+//  Copyright © 2018 Zachary Broeg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ To-Do App/To-Do App/Data Model/Data.swift
@@ -0,0 +1,14 @@
+//
+//  Data.swift
+//  To-Do App
+//
+//  Created by Zachary Broeg on 7/12/18.
+//  Copyright © 2018 Zachary Broeg. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class Data {
+    
+}
