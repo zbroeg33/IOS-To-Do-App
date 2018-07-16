70d0c34146d1b15aa0629ac96e054f0274fa8ca2
Zacharybroeg <Zacharybroeg@u.boisestate.edu>
2018-07-15 20:55:46 -0600

    Built out tableview, and have coredata saving and reading

---
+++ To-Do App/To-Do App/Controller/NotesViewController.swift
@@ -0,0 +1,101 @@
+//
+//  ViewController.swift
+//  To-Do App
+//
+//  Created by Zachary Broeg on 7/12/18.
+//  Copyright © 2018 Zachary Broeg. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class NotesViewController: UITableViewController {
+
+    var arrayItem = [Data]()
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loadItems()
+        
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrayItem.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        let item = arrayItem[indexPath.row]
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done == true ? .checkmark : .none
+        
+        return cell
+    }
+   
+   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+       
+        
+        
+       arrayItem[indexPath.row].done = !arrayItem[indexPath.row].done
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add new Task", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) {
+            (action) in
+            
+            let newItem = Data(context: self.context)
+            newItem.title = textField.text!
+            newItem.done = false
+            //newItem.parentCategory = self.selectedCategory
+            self.arrayItem.append(newItem)
+            
+           
+            self.saveItems()
+            
+        }
+        
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new Task"
+            textField = alertTextField
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    
+    }
+    
+    func saveItems() {
+        
+        do {
+            try self.context.save()
+        } catch {
+            print("Error encoding item array, \(error)")
+        }
+        
+        self.tableView.reloadData()
+    }
+    
+    func loadItems() {
+        let request : NSFetchRequest<Data> = Data.fetchRequest()
+        do {
+         arrayItem = try context.fetch(request)
+        } catch {
+            print("Error fetching Data \(error)")
+        }
+        
+    }
+
+}
+
